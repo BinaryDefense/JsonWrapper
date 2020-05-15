ba2a88324c456ca80b6ef47f09107989e3fecc94
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2020-05-14 20:13:49 -0400

    cleanup old plugin code

--- src/JsonWrapperPlugin/Library.fs
+++ src/JsonWrapperPlugin/Library.fs
@@ -8,70 +8,7 @@ open Myriad.Core
 module internal Create =
     open FSharp.Compiler.Range
 
-    let createFieldMap (parent: LongIdent) (field: SynField)  =
-        let field = field.ToRcd
-        let fieldName = match field.Id with None -> failwith "no field name" | Some f -> f
-
-        let recordType =
-            LongIdentWithDots.Create (parent |> List.map (fun i -> i.idText))
-            |> SynType.CreateLongIdent
-
-        let varName = "x"
-        let pattern =
-            let name = LongIdentWithDots.CreateString fieldName.idText
-            let arg =
-                let named = SynPatRcd.CreateNamed(Ident.Create varName, SynPatRcd.CreateWild )
-                SynPatRcd.CreateTyped(named, recordType)
-                |> SynPatRcd.CreateParen
-
-            SynPatRcd.CreateLongIdent(name, [arg])
-
-        let expr =
-            let ident = LongIdentWithDots.Create [varName; fieldName.idText]
-            SynExpr.CreateLongIdent(false, ident, None)
-
-        let valData =
-            let argInfo = SynArgInfo.CreateIdString "x"
-            let valInfo = SynValInfo.SynValInfo([[argInfo]], SynArgInfo.Empty)
-            SynValData.SynValData(None, valInfo, None)
-
-        SynModuleDecl.CreateLet [{SynBindingRcd.Let with
-                                    Pattern = pattern
-                                    Expr = expr
-                                    ValData = valData }]
-
-    let createCreate (parent: LongIdent) (fields: SynFields) =
-        let varIdent = LongIdentWithDots.CreateString "create"
-
-        let recordType =
-            LongIdentWithDots.Create (parent |> List.map (fun i -> i.idText))
-            |> SynType.CreateLongIdent
-
-        let pattern =
-            let arguments =
-                fields
-                |> List.map (fun f ->let field = f.ToRcd
-                                     let name = SynPatRcd.CreateNamed(field.Id.Value, SynPatRcd.CreateWild)
-                                     SynPatRcd.CreateTyped(name, field.Type) |> SynPatRcd.CreateParen)
-
-            SynPatRcd.CreateLongIdent(varIdent, arguments)
-
-        let expr =
-            let fields =
-                fields
-                |> List.map (fun f ->   let field = f.ToRcd
-                                        let fieldIdent = match field.Id with None -> failwith "no field name" | Some f -> f
-                                        let name = LongIdentWithDots.Create([fieldIdent.idText])
-                                        let ident = SynExpr.CreateIdent fieldIdent
-                                        RecordFieldName(name, true), Some ident, None)
-
-            let newRecord = SynExpr.Record(None, None, fields, range.Zero )
-            SynExpr.CreateTyped(newRecord, recordType)
-
-        let returnTypeInfo = SynBindingReturnInfoRcd.Create recordType
-        SynModuleDecl.CreateLet [{SynBindingRcd.Let with Pattern = pattern; Expr = expr; ReturnInfo = Some returnTypeInfo }]
-
-    let createWrapperClass  (parent: LongIdent) (fields: SynFields) =
+    let createWrapperClass  (parent: LongIdent) (fields: SynFields) (jtokenInterface : string) =
         let jtokenNamespace = "Newtonsoft.Json.Linq.JToken"
         let info = SynComponentInfoRcd.Create parent
         let jtokenIdenName =  "jtoken"
@@ -108,8 +45,6 @@ module internal Create =
 
         let createGetSetMember (fieldName : Ident) (jsonFieldName : string) (ty : SynType) =
             let unitArg = SynPatRcd.Const { SynPatConstRcd.Const = SynConst.Unit ; Range = range.Zero }
-
-
 
             let getMemberExpr =
                 //Generates the jtoken.["jsonFieldName"]
@@ -185,7 +120,7 @@ module internal Create =
                 createGetSetMember fieldIdent jsonFieldName frcd.Type
             )
 
-        let createInterfaceImpl () =
+        let createInterfaceImpl (jtokenInterface : string) =
             let implementedMembers =
                 let createInnerDataMemberVal  =
                     let memberFlags : MemberFlags = {
@@ -204,113 +139,31 @@ module internal Create =
                 }
 
                 [ SynMemberDefn.CreateMember(bindingRecord)]
-            SynMemberDefn.Interface(SynType.CreateLongIdent("Example.IHaveJToken"), Some implementedMembers, range0)
+            SynMemberDefn.Interface(SynType.CreateLongIdent(jtokenInterface), Some implementedMembers, range0)
 
         let members = [
             createCtor ()
             yield! createGetSetMembersFromRecord ()
-            createInterfaceImpl ()
+            createInterfaceImpl jtokenInterface
         ]
 
         SynModuleDecl.CreateType(info, members)
 
-    let createMap (recordId: LongIdent) (recordFields: SynFields) : SynModuleDecl =
-        let varIdent = LongIdentWithDots.CreateString "map"
-        let recordPrimeIdent =  Ident.Create "record'"
-
-        let createFieldMapNameIdent field =
-            Ident.Create ("map" + field.Id.Value.idText)
-
-        let pattern =
-            let arguments =
-                recordFields
-                |> List.map (fun f ->let field = f.ToRcd
-                                     let fieldType = field.Type
-                                     let funType = SynType.Fun(fieldType, fieldType, range0 )
-                                     let ident = createFieldMapNameIdent field
-                                     let name = SynPatRcd.CreateNamed(ident, SynPatRcd.CreateWild)
-                                     SynPatRcd.CreateTyped(name, funType)
-                                     |> SynPatRcd.CreateParen)
-
-            let recordParam =
-                let name = SynPatRcd.CreateNamed(recordPrimeIdent, SynPatRcd.CreateWild)
-                let typ =
-                    LongIdentWithDots.Create (recordId |> List.map (fun i -> i.idText))
-                    |> SynType.CreateLongIdent
-
-                SynPatRcd.CreateTyped(name, typ)
-                |> SynPatRcd.CreateParen
-
-            let allArgs = [yield! arguments; yield recordParam]
-
-            SynPatRcd.CreateLongIdent(varIdent, allArgs)
-
-        let expr =
-            let copyInfo =
-                let blockSep = (range.Zero, None) : BlockSeparator
-                Some (SynExpr.CreateIdent recordPrimeIdent, blockSep)
-
-            let fieldUpdates =
-                let mapField (f: SynField) =
-                    let f = f.ToRcd
-                    let lid = LongIdentWithDots.Create [f.Id.Value.idText]
-                    let rfn = RecordFieldName(lid, true)
-
-                    let update =
-                        let funcExpr = SynExpr.CreateIdent (createFieldMapNameIdent f)
-                        let argExpr =
-                            let longIdentWithDots = LongIdentWithDots.Create [recordPrimeIdent.idText; f.Id.Value.idText]
-                            SynExpr.CreateLongIdent(false, longIdentWithDots, None)
-                        SynExpr.CreateApp(funcExpr, argExpr)
-
-                    rfn, Some update, (None : Option<BlockSeparator>)
-
-                let arguments =
-                    recordFields
-                    |> List.map mapField
-
-                arguments
-
-            SynExpr.Record(None, copyInfo, fieldUpdates, range.Zero )
-
-        let returnTypeInfo =
-            LongIdentWithDots.Create (recordId |> List.map (fun i -> i.idText))
-            |> SynType.CreateLongIdent
-            |> SynBindingReturnInfoRcd.Create
-
-        SynModuleDecl.CreateLet
-            [ { SynBindingRcd.Let with
-                    Pattern = pattern
-                    Expr = expr
-                    ReturnInfo = Some returnTypeInfo }
-            ]
 
     let createRecordModule (namespaceId: LongIdent) (typeDefn: SynTypeDefn) =
         let (TypeDefn(synComponentInfo, synTypeDefnRepr, _members, _range)) = typeDefn
         let (ComponentInfo(_attributes, _typeParams, _constraints, recordId, _doc, _preferPostfix, _access, _range)) = synComponentInfo
+
+        let jtokenInferface = "Example.IHaveJToken" //TODO: Scan and get fully qualified
         match synTypeDefnRepr with
         | SynTypeDefnRepr.Simple(SynTypeDefnSimpleRepr.Record(_accessibility, recordFields, _recordRange), _range) ->
 
-            let openParent = SynModuleDecl.CreateOpen (LongIdentWithDots.Create (namespaceId |> List.map (fun ident -> ident.idText)))
-
-            let fieldMaps = recordFields |> List.map (createFieldMap recordId)
-
-            let create = createCreate recordId recordFields
-
-            let map = createMap recordId recordFields
-
-            let createWrapperClass = createWrapperClass recordId recordFields
+            let createWrapperClass = createWrapperClass recordId recordFields jtokenInferface
 
             let declarations = [
-                // yield openParent
-                // yield!fieldMaps
-                // yield create
-                // yield map
                 createWrapperClass
             ]
 
-            // let info = SynComponentInfoRcd.Create recordId
-            // SynModuleDecl.CreateNestedModule(info, declarations)
             declarations
         | _ -> failwithf "Not a record type"
 
