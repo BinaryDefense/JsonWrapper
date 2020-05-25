d315af080825e2f1b2cc41d0e8f76597f1b55b82
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2020-05-25 15:23:18 -0400

    fix build errors

--- build.fsx
+++ build.fsx
@@ -75,7 +75,7 @@ let docsSrcDir = __SOURCE_DIRECTORY__  @@ "docsSrc"
 let docsToolDir = __SOURCE_DIRECTORY__ @@ "docsTool"
 
 let gitOwner = "BinaryDefense"
-let gitRepoName = "BinaryDefense.JsonWrapper"
+let gitRepoName = "JsonWrapper"
 
 let gitHubRepoUrl = sprintf "https://github.com/%s/%s" gitOwner gitRepoName
 
