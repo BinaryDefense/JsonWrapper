7764a0cb0398b4b37cc03bfc319c84efacb57dfc
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2020-05-26 13:24:43 -0400

    Bump version to 0.1.0-beta004
    ## [0.1.0-beta004] - 2020-05-26
    
    ### Added
    - Initial release
    [Unreleased]: https://github.com/BinaryDefense/JsonWrapper/compare/v0.1.0-beta003...HEAD
    [0.1.0-beta003]: https://github.com/BinaryDefense/JsonWrapper/releases/tag/v0.1.0-beta003
    [0.1.0-beta002]: https://github.com/BinaryDefense/BinaryDefense.JsonWrapper/releases/tag/v0.1.0-beta002
    [0.1.0-beta001]: https://github.com/BinaryDefense/BinaryDefense.JsonWrapper/releases/tag/v0.1.0-beta001

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,22 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.0-beta004] - 2020-05-26
+
+### Added
+- Initial release
+[Unreleased]: https://github.com/BinaryDefense/JsonWrapper/compare/v0.1.0-beta003...HEAD
+[0.1.0-beta003]: https://github.com/BinaryDefense/JsonWrapper/releases/tag/v0.1.0-beta003
+[0.1.0-beta002]: https://github.com/BinaryDefense/BinaryDefense.JsonWrapper/releases/tag/v0.1.0-beta002
+[0.1.0-beta001]: https://github.com/BinaryDefense/BinaryDefense.JsonWrapper/releases/tag/v0.1.0-beta001
+
 ## [0.1.0-beta003] - 2020-05-25
+
 ### Added
 - Initial release
 
-[Unreleased]: https://github.com/BinaryDefense/JsonWrapper/compare/v0.1.0-beta003...HEAD
+[Unreleased]: https://github.com/BinaryDefense/JsonWrapper/compare/v0.1.0-beta004...HEAD
+[0.1.0-beta004]: https://github.com/BinaryDefense/JsonWrapper/releases/tag/v0.1.0-beta004
 [0.1.0-beta003]: https://github.com/BinaryDefense/JsonWrapper/releases/tag/v0.1.0-beta003
 [0.1.0-beta002]: https://github.com/BinaryDefense/BinaryDefense.JsonWrapper/releases/tag/v0.1.0-beta002
 [0.1.0-beta001]: https://github.com/BinaryDefense/BinaryDefense.JsonWrapper/releases/tag/v0.1.0-beta001
--- src/BinaryDefense.Myriad.Plugins.JsonWrapper/AssemblyInfo.fs
+++ src/BinaryDefense.Myriad.Plugins.JsonWrapper/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("BinaryDefense.Myriad.Plugins.JsonWrapper")>]
 [<assembly: AssemblyProductAttribute("BinaryDefense.JsonWrapper")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-05-25T00:00:00.0000000-04:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-05-26T00:00:00.0000000-04:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","d315af080825e2f1b2cc41d0e8f76597f1b55b82")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","0fdc94a755c30872e1abfef64fb1a08503abe96e")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "BinaryDefense.Myriad.Plugins.JsonWrapper"
     let [<Literal>] AssemblyProduct = "BinaryDefense.JsonWrapper"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-05-25T00:00:00.0000000-04:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-05-26T00:00:00.0000000-04:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
-    let [<Literal>] AssemblyMetadata_GitHash = "d315af080825e2f1b2cc41d0e8f76597f1b55b82"
+    let [<Literal>] AssemblyMetadata_GitHash = "0fdc94a755c30872e1abfef64fb1a08503abe96e"
